24d653d609e177a36ae977efe7e5a30f7fd614b0
mebratu <mebratucheka7@gmail.com>
2026-08-15 11:33:36 +0300

    MCP client with DuckDuckGo MCP server

---
+++ mcp_client.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+import certifi
+from dotenv import load_dotenv
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+load_dotenv()
+
+# create the client and connect to the MCP servers
+MCP_client = MultiServerMCPClient(
+    {
+        # DuckDuckGo MCP server
+        "ddg-search": {
+            "command": "uvx",
+            "args": ["duckduckgo-mcp-server"],
+            "transport": "stdio", # stdio, sse, websocket, http
+            # "url": "https://html.duckduckgo.com/html"
+        },
+
+
+    }
+)
+
+# get tools in the MCP client
+async def get_all_mcp_tools():
+    tools = await MCP_client.get_tools()
+    print("Connected MCP tools:")
+    for tool in tools:
+        print(f"- {tool.name}")
